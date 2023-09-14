d872be383e29589bfc6656bdbc63aa9ecd523aeb
soumyo1999 <soumyo.deepto.dash@gmail.com>
2023-09-14 10:47:51 +0530

    React js Day 1 adding html layout

---
+++ src/Components/Aside.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Aside() {
+  return (
+    <aside>
+        <h1>SideBar Component</h1>
+    </aside>
+  )
+}
+
+export default Aside
---
+++ src/Components/Section.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Section() {
+  return (
+    <my-section>
+          <h1>Content Component</h1>
+    </my-section>
+  )
+}
+
+export default Section
